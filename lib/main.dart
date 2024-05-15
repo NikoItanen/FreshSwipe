3b9dc35a0ac720b7bd643b71d5fca24c1282a33e
Niko Itänen <120341039+NikoItanen@users.noreply.github.com>
2024-05-15 23:11:14 +0300

    Login page structure implemented.
    - Changed the background colour.
    
    - Added "Show password" button and functionality.
    
    - Added "Register" button. (Functionality not yet added)
    
    - Slightly corrected the size of the login button.
    
    - Minor corrections to other colors on the page.

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,31 @@ void main() {
   runApp(const FreshSwipe());
 }
 
-class FreshSwipe extends StatelessWidget {
+class FreshSwipe extends StatefulWidget {
   const FreshSwipe({super.key});
+
+  @override
+  State<FreshSwipe> createState() => _FreshSwipeState();
+}
+
+class _FreshSwipeState extends State<FreshSwipe> {
+
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Title for the application
       title: 'FreshSwipe',
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 205, 246, 164),
         body: Center(
           heightFactor: 3.5,
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             // Title text to the login page.
             const Text('FreshSwipe',
                 style: TextStyle(
-                    fontSize: 48, color: Color.fromRGBO(64, 81, 47, 100))),
+                    fontSize: 48, color: Color.fromRGBO(64, 81, 47, 1))),
             const SizedBox(height: 20),
             // Username input field.
             const Padding(
@@ -32,25 +42,42 @@ class FreshSwipe extends StatelessWidget {
                   ),
                 )),
             //Password input field.
-            const Padding(
-                padding: EdgeInsets.all(10.0),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
                 child: SizedBox(
                   width: 400,
                   child: TextField(
-                    obscureText: true,
+                    obscureText: _obscureText,
                     decoration: InputDecoration(
                       hintText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.remove_red_eye),
+                        onPressed: () {
+                          setState(() {
+                            _obscureText = !_obscureText;
+                          });
+                        },
+                      )
                     ),
                   ),
                 )),
             // Log in button
             TextButton(
               style: TextButton.styleFrom(
-                  foregroundColor: Colors.white, backgroundColor: Colors.black),
+                  foregroundColor: Colors.white, backgroundColor: Colors.black, fixedSize: const Size(170, 40)),
               child: const Text('Log In'),
               onPressed: () {
                 // TODO: Make the login logic here
-                print("Login painettu");
+                print("Login button pressed!");
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                  foregroundColor: const Color.fromARGB(255, 0, 0, 0)),
+              child: const Text('Create a new account!'),
+              onPressed: () {
+                // TODO: Make the login logic here
+                print("Register button pressed!");
               },
             ),
           ]),
