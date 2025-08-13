6a94972f89e516ecc13fd938cbe651a8841e61b4
Niko Itänen <120341039+NikoItanen@users.noreply.github.com>
2025-08-13 14:04:53 +0300

    Little modifications in settings and initialization code

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,26 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
 Future<void> main() async {
-  // Initialize Firebase options.
-  await dotenv.load(fileName: ".env");
+
+  // Ensure that the Flutter binding is initialized.
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load the .env file.
+  await dotenv.load(fileName: ".env");
+
+  // Initialize Firebase.
   await Firebase.initializeApp(options: FirebaseOptions(
-  apiKey: '${dotenv.env['FIREBASE_API_KEY']}',
-  authDomain: "${dotenv.env['FIREBASE_AUTH_DOMAIN']}",
-  projectId: "${dotenv.env['FIREBASE_PROJECT_ID']}",
-  storageBucket: "${dotenv.env['FIREBASE_STORAGE_BUCKET']}",
-  messagingSenderId: "${dotenv.env['FIREBASE_MESSAGING_SENDER_ID']}",
-  appId: "${dotenv.env['FIREBASE_APP_ID']}",
-  measurementId: "${dotenv.env['FIREBASE_MEASUREMENT_ID']}"));
+    apiKey: dotenv.env['FIREBASE_API_KEY']!,
+    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
+    databaseURL: dotenv.env['FIREBASE_DATABASE_URL']!,
+    projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+    appId: dotenv.env['FIREBASE_APP_ID']!,
+    measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID']!,
+  ));
+
+ // Run the app.
   runApp(const FreshSwipe());
 }
 
